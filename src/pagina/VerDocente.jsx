1d8f2a51df498b1c50d2c9e71015b566ad6d4072
CarlosJuanta <cjuantat@miumg.edu.gt>
2023-09-04 19:24:51 -0600

    busquedas ok

--- src/pagina/VerDocente.jsx
+++ src/pagina/VerDocente.jsx
@@ -1,83 +1,164 @@
 import React, { useState, useEffect } from "react";
-import { FormGroup, Label, Input, Col, Row, Button, Form } from "reactstrap";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Row,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
 
-import {NavLink} from 'react-router-dom'
-import * as FaIcons from 'react-icons/fa'
 const VerDocente = () => {
-  const [datos, setDatos] = useState("");
+  const [modal, setModal] = useState(false);
+  const [selectedDocente, setSelectedDocente] = useState();
+  const [filtroNombre, setFiltroNombre] = useState("");
+  const [datos, setDatos] = useState([]);
+  const [busquedaRealizada, setBusquedaRealizada] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const handleVerClick = (docente) => {
+    setSelectedDocente(docente);
+    toggleModal();
+  };
 
   const getDocentes = async () => {
     try {
       const response = await fetch(`${"http://localhost:3000/api/"}/docente/getall`);
       const data = await response.json();
-      setDatos(data.resultado);
-      console.log(data.resultado);
+      // Filtra los docentes por nombre si se ha ingresado un valor en el campo de búsqueda
+      const docentesFiltrados = filtroNombre
+        ? data.resultado.filter((docente) =>
+            docente.nombreDocente.toLowerCase().includes(filtroNombre.toLowerCase())
+          )
+        : [];
+      setDatos(docentesFiltrados);
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    getDocentes();
-  }, []);
-
-  if (datos.length === 0) {
-    return <h1>Cargando...</h1>;
-  }
   return (
     <>
-     <h4>Docente</h4>
-     <div className="p-5">
-     <Row>
-      <Col>
-        <NavLink to="/docente">
-          <Button color="success">
-            Registrar Docente
+      <h4>Docente</h4>
+      <div className="p-5">
+      <Row>
+       <Col > 
+          <Button
+          color="primary"
+          onClick={() => {
+          setBusquedaRealizada(true);
+          getDocentes();
+          }}
+          >
+          Buscar
           </Button>
-        </NavLink>
-      </Col>
-      <Col className="text-end">
-        <Input 
-        placeholder = "Buscar Docente"
-        type="text-area"
-        />
-      </Col>
-      
-    </Row>
-     </div>
-    <div class="table-responsive p-5">
-    <table class="table table-hover table-light table-sm align-middle table-striped">
-  <thead class="table-dark table text-center">
-  
-    <tr>
-      <th scope="col">CUI</th>
-      <th scope="col">Nombres </th>
-      <th scope="col">Apellidos</th>
-      <th scope="col">Teléfono</th>
-      <th scope="col">Correo</th>
-      <th scope="col">Dirección</th>
-      <th scope="col">Nacionalidad</th>
-      <th scope="col">Grados Asignados</th>
-    </tr>
-  </thead>
-  <tbody class="table text-center">
-  {datos.map((docente) => (
-            <tr key={docente.cuiDocente}>
-              <td>{docente.cuiDocente}</td>
-              <td>{docente.nombreDocente}</td>
-              <td>{docente.apellidoDocente}</td>
-              <td>{docente.telefonoDocente}</td>
-              <td>{docente.correoDocente}</td>
-              <td>{docente.direccionDocente}</td>
-              <td>{docente.nacionalidadDocente}</td>
-              <td><NavLink to="/gradodocente" className='text-dark rounded py-2 w-100 d-inline-block px-2 ' activeClassName='active'  style={{ textDecoration: 'none' }} ><FaIcons.FaEye className="me-2"/>Ver</NavLink> </td>
-            </tr>
-          ))}
-  </tbody>
-</table> 
-</div>
-    </>
-  )
-}
+          </Col>
+       </Row>
+        <Row>
+          <Col sm={12} md={6}>
+          <Input
+              placeholder="Buscar Docente por Nombre"
+              type="text"
+              value={filtroNombre}
+              onChange={(e) => setFiltroNombre(e.target.value)}
+            />
+            </Col>
+          <Col className="text-end">
+          <NavLink to="/docente">
+              <Button color="success">Registrar Docente</Button>
+            </NavLink>
+          </Col>
+         </Row>
+      </div>
+      <div className="table-responsive p-5">
+        {datos.length > 0 ? (
+          <table className="table table-hover table-light table-sm align-middle table-striped">
+            {/* El contenido de la tabla se mostrará solo si hay datos */}
+            <thead className="table-dark table text-center">
+              <tr>
+                <th scope="col">CUI</th>
+                <th scope="col">Nombres</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Teléfono</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Dirección</th>
+                <th scope="col">Nacionalidad</th>
+                <th scope="col">Grados Asignados</th>
+                <th scope="col">Acciones</th>
+              </tr>
+            </thead>
+            <tbody className="table text-center">
+              {datos.map((docente) => (
+                <tr key={docente.cuiDocente}>
+                  <td>{docente.cuiDocente}</td>
+                  <td>{docente.nombreDocente}</td>
+                  <td>{docente.apellidoDocente}</td>
+                  <td>{docente.telefonoDocente}</td>
+                  <td>{docente.correoDocente}</td>
+                  <td>{docente.direccionDocente}</td>
+                  <td>{docente.nacionalidadDocente}</td>
+                  <td>
+                    <NavLink
+                      to="/gradodocente"
+                      className="text-dark rounded py-2 w-100 d-inline-block px-2"
+                      activeClassName="active"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <FaIcons.FaEye className="me-2" /> Ver
+                    </NavLink>
+                  </td>
+                  <td>
+                    <Button
+                      color="success"
+                      onClick={() => {
+                        handleVerClick(docente);
+                      }}
+                    >
+                      <FaIcons.FaEye className="me-2" />
+                      Ver
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No se encontraron resultados.</p>
+        )}
+      </div>
 
-export default VerDocente
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Detalles del Docente</ModalHeader>
+        <ModalBody>
+          {selectedDocente && (
+            <>
+              <p><strong>CUI:</strong> {selectedDocente.cuiDocente}</p>
+              <p><strong>Nombres:</strong> {selectedDocente.nombreDocente}</p>
+              <p><strong>Apellidos:</strong> {selectedDocente.apellidoDocente}</p>
+              <p><strong>Teléfono:</strong> {selectedDocente.telefonoDocente}</p>
+              <p><strong>Correo:</strong> {selectedDocente.correoDocente}</p>
+              <p><strong>Dirección:</strong> {selectedDocente.direccionDocente}</p>
+              <p><strong>Nacionalidad:</strong> {selectedDocente.nacionalidadDocente}</p>
+            </>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggleModal}>
+            Cerrar
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+};
+
+export default VerDocente;
--- src/pagina/VerEstudiante.jsx
+++ src/pagina/VerEstudiante.jsx
@@ -1,86 +1,151 @@
 import React, { useState, useEffect } from "react";
-import { FormGroup, Label, Input, Col, Row, Button, Form } from "reactstrap";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Row,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
 import { NavLink } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 
-
 const VerEstudiante = () => {
+  const [modal, setModal] = useState(false);
+  const [selectedEncargado, setSelectedEncargado] = useState();
+  const [filtroNombre, setFiltroNombre] = useState("");
   const [datos, setDatos] = useState([]);
+  const [busquedaRealizada, setBusquedaRealizada] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const handleVerClick = (estudiante) => {
+    setSelectedEncargado(estudiante);
+    toggleModal();
+  };
 
   const getEstudiantes = async () => {
     try {
-      const response = await fetch(`${"http://localhost:3000/api/"}/estudiante/getall`);
-      const data = await response.json();
-      setDatos(data.resultado);
-      console.log(data.resultado);
+      if (busquedaRealizada) {
+        const response = await fetch(`${"http://localhost:3000/api/"}/estudiante/getall`);
+        const data = await response.json();
+        const estudiantesFiltrados = filtroNombre
+          ? data.resultado.filter((estudiante) =>
+              new RegExp(filtroNombre, 'i').test(estudiante.nombreEstudiante)
+            )
+          : [];
+        setDatos(estudiantesFiltrados);
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    getEstudiantes();
-  }, []);
-
-  if (datos.length === 0) {
-    return <h1>Cargando...</h1>;
-  }
-
   return (
     <>
       <h4>Estudiante</h4>
       <div className="p-5">
-        <Row>
-          <Col>
+       <Row>
+       <Col > 
+          <Button
+          color="primary"
+          onClick={() => {
+          setBusquedaRealizada(true);
+          getEstudiantes();
+          }}
+          >
+          Buscar
+          </Button>
+          </Col>
+       </Row>
+          <Row >
+       <Col sm={12} md={6} >
+    <Input
+      placeholder="Buscar Estudiante por Nombre"
+      type="text"
+      value={filtroNombre}
+      onChange={(e) => setFiltroNombre(e.target.value)}
+          />
+          </Col>
+          
+          <Col className="text-end" >
             <NavLink to="/estudiante">
               <Button color="success">Crear Estudiante</Button>
             </NavLink>
-          </Col>
-          <Col className="text-end">
-            <Input placeholder="Buscar Estudiante" type="text-area" />
-          </Col>
-        </Row>
+          </Col >
+          </Row>
       </div>
       <div className="table-responsive p-5">
-        <table className="table table-hover table-light table-sm align-middle table-striped">
-          <thead className="table-dark table text-center">
-            <tr>
-              <th scope="col">CUI</th>
-              <th scope="col">Nombre </th>
-              <th scope="col">Apellido</th>
-              <th scope="col">Fecha de Nacimiento</th>
-              <th scope="col">Dirección</th>
-              <th scope="col">Nacionalidad</th>
-              <th scope="col">Código MINEDUC</th>
-              <th scope="col">Encargado</th>
-            </tr>
-          </thead>
-          <tbody className="table text-center">
-            {datos.map((estudiante) => (
-              <tr key={estudiante.cuiEstudiante}>
-                <td>{estudiante.cuiEstudiante}</td>
-                <td>{estudiante.nombreEstudiante}</td>
-                <td>{estudiante.apellidoEstudiante}</td>
-                <td>{estudiante.fechanacEstudiante}</td>
-                <td>{estudiante.direccionEstudiante}</td>
-                <td>{estudiante.nacionalidadEstudiante}</td>
-                <td>{estudiante.codigomineducEstudiante}</td>
-                <td>{estudiante.correencargadoEstudiante}</td>
-                <td>
-                  <NavLink
-                    to={`/encargadoinfo/${estudiante._id}`}
-                    className="text-dark rounded py-2 w-100 d-inline-block px-2"
-                    activeClassName="active"
-                    style={{ textDecoration: "none" }}
-                  >
-                    <FaIcons.FaEye className="me-2" /> Ver
-                  </NavLink>
-                </td>
+        {datos.length > 0 ? (
+          <table className="table table-hover table-light table-sm align-middle table-striped">
+            <thead className="table-dark table text-center">
+              <tr>
+                <th scope="col">CUI</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Fecha de Nacimiento</th>
+                <th scope="col">Dirección</th>
+                <th scope="col">Nacionalidad</th>
+                <th scope="col">Código MINEDUC</th>
+                <th scope="col">Encargado</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="table text-center">
+              {datos.map((estudiante) => (
+                <tr key={estudiante.cuiEstudiante}>
+                  <td>{estudiante.cuiEstudiante}</td>
+                  <td>{estudiante.nombreEstudiante}</td>
+                  <td>{estudiante.apellidoEstudiante}</td>
+                  <td>{estudiante.fechanacEstudiante}</td>
+                  <td>{estudiante.direccionEstudiante}</td>
+                  <td>{estudiante.nacionalidadEstudiante}</td>
+                  <td>{estudiante.codigomineducEstudiante}</td>
+                  <td>
+                    <Button
+                      color="success"
+                      onClick={() => {
+                        handleVerClick(estudiante);
+                      }}
+                    >
+                      <FaIcons.FaEye className="me-2" />
+                      Ver
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No se encontraron resultados.</p>
+        )}
       </div>
+
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Detalles del Encargado</ModalHeader>
+        <ModalBody>
+          {selectedEncargado && (
+            <>
+              <p><strong>CUI:</strong> {selectedEncargado.cuiencargadoEstudiante}</p>
+              <p><strong>Nombre:</strong> {selectedEncargado.nombreencargadoEstudiante}</p>
+              <p><strong>Apellido:</strong> {selectedEncargado.apellidoencargadoEstudiante}</p>
+              <p><strong>Dirección:</strong> {selectedEncargado.direccionencargadoEstudiante}</p>
+              <p><strong>Teléfono:</strong> {selectedEncargado.telefonoencargadoEstudiante}</p>
+              <p><strong>Correo:</strong> {selectedEncargado.correencargadoEstudiante}</p>
+            </>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggleModal}>
+            Cerrar
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
